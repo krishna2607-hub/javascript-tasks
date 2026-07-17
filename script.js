27a640c6cbf5453c93e68e1842d947fd9153c324
krishna2607-hub <nanikrishna2607@gmail.com>
2026-07-17 07:05:37 +0530

    employee registration and various tasks in script.js

---
+++ script.js
@@ -0,0 +1,164 @@
+// Task 1: Employee Registration
+const companyName = "Stackly";
+let employeeId = "STK-26-3911";
+let employeeName = prompt("Enter Employee Name");
+
+console.log("companyName :", companyName);
+console.log("Employee Name :", employeeName);
+console.log("Employee ID :", employeeId);
+
+//Task 2: College Admission
+const CollegeName = "CBIT";
+let StudentName = prompt("Enter the Student Name");
+let Course = prompt("Enter The Course Details")
+
+console.log("CollegeName :", CollegeName);
+console.log("StudentName ;", StudentName );
+console.log("Course :", Course);
+
+//Task 3: Food Order
+
+const Restaurant = "ERODE AMAAN MESS";
+let CustomerName = prompt("Enter the Customer Name");
+let FoodName = prompt("Enter The Course Details")
+let Quantity = 2;
+
+console.log("Restaurant :", Restaurant);
+console.log("CustomerName :", CustomerName );
+console.log("FoodName :", FoodName);
+console.log("Quantity :", quantity );
+
+
+//Task 4: Hospital Registration
+const HospitalName = "GITAM";
+let PatientName = prompt("Enter the Patient Name");
+let TokenNumber = 26;
+
+console.log("HospitalName :", CustomerName);
+console.log("PatientName :", PatientName);
+console.log("TokenNumber :",tokenNumber);
+
+
+//Task 5: Movie Ticket Booking
+const TheatreName = "INOX";
+let MovieName = prompt("Enter the Movie Name");
+let CustomerName = prompt("Enter the Customer Name");
+let SeatNumber = "A25" ;
+
+console.log("TheatreName :", TheatreName );
+console.log("MovieName :", MovieName);
+console.log("companyName :", CustomerName);
+console.log("SeatNumber :",SeatNumber);
+
+
+//Task 6: Mobile Shop
+const ShopName = "CK Mobile";
+let CustomerName = prompt("Enter The Customer Name");
+let MobileModel = prompt ("Enter The Mobile Model");
+let Price = 30000;
+
+console.log("ShopName :", ShopName);
+console.log("CustomerName :", CustomerName);
+console.log("MobileModel :", MobileModel);
+console.log("Price :", Price);
+
+
+//Task 7: Gym Membership
+
+const GymName = "Gold Gym";
+let MemberName=prompt("Enter The Member Name");
+let MembershipFee = 10000;
+
+console.log("GymName :", GymName);
+console.log("MemberName :", MemberName);
+console.log("MembershipFee :", MembershipFee);
+
+
+//Task 8: ATM Welcome Screen
+const BankName = "SBI";
+let UserName = prompt("Enter The User Name");
+
+console.log("BankName :", BankName);
+console.log("UserName :", UserName);
+
+//Task 9: Bus Ticket
+const TravelsName ="Zing";
+let PassengerName = prompt("Enter The Passenger Name");
+let SeatNumber = 22;
+
+console.log("TravelsName :",TravelsName);
+console.log("PassengerName :",PassengerName);
+console.log("SeatNumber :",SeatNumber);
+
+
+//Task 10: Online Shopping
+const WebSideNAme ="Flipkart";
+let ProductName = prompt("Enter the Product Name");
+let CustomerName= prompt("Enter The Customer Name");
+let Price =1500;
+
+console.log ("WebSideNAme :", WebSideNAme);
+console.log ("ProductName :",PatientName);
+console.log ("CustomerName :",CustomerName);
+console.log ("Price :",Price);
+
+
+//Console Method Tasks
+// Task 11;  success message using console.log().
+console.log("Registration Successful");
+
+
+//Task 12; error using console.error().
+console.error("Payment Failed");
+
+
+//ask 13; warning using console.warn().
+console.warn("Battery Low");
+
+
+//Task 14; some values and then clear the console using console.clear().
+console.log("Loading...");
+console.log("Welcome");
+
+console.clear();
+
+
+
+//Task 15; Alert, Confirm & Prompt Tasks
+
+alert("Welcome to Amazon");
+
+
+
+//Task 16; Do you want to logout?  using confirm().
+let logout = confirm("Do you want to logout?");
+
+console.log(logout);
+
+
+
+//Task 17; Ask the user's city using prompt().
+let city = prompt("Enter Your City");
+
+console.log("Your City :", city);
+
+
+//Task 18; Variable Practice
+var product = "Laptop";
+product = "Mobile";
+var product = "Tablet";
+
+console.log(product);
+
+
+//Task 19;
+let salary = 40000;
+
+salary = 30000;
+
+console.log(salary);
+
+
+//Task 20;
+const Company ="Apex";
+console.log("Company :",company);
